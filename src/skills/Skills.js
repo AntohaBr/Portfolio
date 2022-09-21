58573544787309fe95028e17e170f1024b8161dd
AntohaBr <anton.brel@mail.ru>
2022-09-21 17:42:18 +0300

    footer/1

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -9,9 +9,8 @@ function Skills() {
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Skills</h2>
                 <div className={style.skills}>
-                    <Skill title={'Js'} description={'Modern JavaScript Tutorial: simple, but detailed explanations with examples and tasks, including: closures, document and events, object oriented programming and more.'}/>
-                    <Skill title={'Css'}description={'Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML '}/>
-                    <Skill title={'React'} description={'React makes it painless to create interactive UIs.' }/>
+                    <Skill title={'Js'} description={'Modern JavaScript Tutorial: simple, but detailed explanations with examples and tasks, including: closures, document and events, object oriented programming and more.React makes it painless to create interactive UIs.React makes it painless to create interactive UIs.React makes it painless to create interactive UIs.React makes it painless to create interactive UIs.'}/>
+                    <Skill title={'React'} description={'React makes it painless to create interactive UIs. React makes it painless to create interactive UIs.React makes it painless to create interactive UIs.React makes it painless to create interactive UIs.React makes it painless to create interactive UIs.React makes it painless to create interactive UIs.' }/>
                 </div>
             </div>
         </div>
