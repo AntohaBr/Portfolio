d610cd16d24f0d35ee4bcad3e10e51bc25ce1e42
AntohaBr <anton.brel@mail.ru>
2022-09-21 10:09:18 +0300

    block 6

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './Contacts.module.css';
 import styleContainer from "../common/styles/Container.module.css";
-import InputBox from "../inputBox/InputBox";
+
 
 
 function Contact() {
@@ -9,7 +9,13 @@ function Contact() {
         <div className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
                 <h2 className={style.title}>Contact</h2>
-                <InputBox/>
+                <div className={style.inputBlock}>
+                    <form>
+                        <div className={style.inputBoxOne}><input className={style.inputOne}/></div>
+                        <div className={style.inputBoxToo}><input className={style.inputToo}/></div>
+                        <div className={style.textareaBox}><textarea className={style.textarea}/></div>
+                    </form>
+                </div>
                 <button className={style.button}>Send Message</button>
             </div>
         </div>
