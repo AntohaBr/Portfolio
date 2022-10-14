54166878448aedde08789c7889fde13d2fe4633c
AntohaBr <anton.brel@mail.ru>
2022-10-14 14:32:57 +0300

    refactor-1

--- src/jobs/Jobs.js
+++ src/jobs/Jobs.js
@@ -8,7 +8,9 @@ function Jobs() {
     return (
         <div className={style.jobsBlock}>
             <div className={`${styleContainer.container} ${style.jobsContainer}`}>
-                <h2 className={style.title}>Jobs</h2>
+                <div className={style.title}>
+                    <h2 >Projects</h2>
+                </div>
                 <div className={style.jobs}>
                     <Job  title={'Todolist'} description={'Todolist to optimize working hours.'} />
                     <Job  title={'Social network'} description={'Social network for meeting people and communicating with them.'}/>
