4793bb407157e8b2c22fbcb1cae81249667d3495
AntohaBr <brelanton@gmail.com>
2022-11-10 21:29:37 +0300

    add img

--- src/jobs/Jobs.js
+++ src/jobs/Jobs.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import style from './Jobs.module.css';
-import Job from "./job/Job";
-import styleContainer from "../common/styles/Container.module.css";
-import Title from "../common/components/title/Title.js";
-
-
+import Job from './job/Job';
+import styleContainer from '../common/styles/Container.module.css';
+import Title from '../common/components/title/Title.js';
+import socialImg from '../assets/image/social_network_img.jpg'
+import todolistImg from '../assets/image/todolost_img.jpg'
 
 function Jobs() {
+    const todolist = {
+        color: 'blue',
+        backgroundImage: 'url(&`todolistImg`)',
+    };
+
+    const socialNetwork = {
+        color: 'blue',
+        backgroundImage: 'url(' + imgUrl + ')',
+    };
+
     return (
         <div className={style.jobsBlock}>
             <div className={`${styleContainer.container} ${style.jobsContainer}`}>
