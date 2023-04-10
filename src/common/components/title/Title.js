fda2526017ce880a250abb20233d98d1135100e1
AntohaBr <brelanton@gmail.com>
2023-04-10 14:59:20 +0300

    fix: updating the version node.js

--- src/common/components/title/Title.js
+++ src/common/components/title/Title.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import style from './Title.module.css';
+import React from 'react'
+import s from './Title.module.scss'
+
 
 function Title(props) {
     return (
-        <div className={style.title}>
+        <div className={s.title}>
             <h2>{props.text}</h2>
         </div>
-    );
+    )
 }
 
-export default Title;
+export default Title
--- src/jobs/Jobs.js
+++ src/jobs/Jobs.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import style from './Jobs.module.css';
-import Job from './job/Job';
-import styleContainer from '../common/styles/Container.module.css';
-import Title from '../common/components/title/Title.js';
+import React from 'react'
+import s from './Jobs.module.scss'
+import Job from './job/Job'
+import styleContainer from '../common/styles/Container.module.css'
+import Title from '../common/components/title/Title.js'
 import socialImg from '../assets/image/social_network_img.jpg'
 import todolistImg from '../assets/image/todolost_img.jpg'
+
 
 function Jobs() {
     const todolist = {
         backgroundImage: `url(${todolistImg})`
-    };
+    }
 
     const socialNetwork = {
         backgroundImage: `url(${socialImg})`
-    };
+    }
 
     return (
-        <div className={style.jobsBlock}>
-            <div className={`${styleContainer.container} ${style.jobsContainer}`}>
+        <div className={s.jobsBlock}>
+            <div className={`${styleContainer.container} ${s.jobsContainer}`}>
                <Title text={"Projects"}/>
-                <div className={style.jobs}>
-                    <Job style={todolist} title={'Todolist'} description={'Todolist to optimize working hours.'} />
+                <div className={s.jobs}>
+                    <Job style={todolist} title={'To Do List'} description={'Todolist to optimize working hours.'} />
                     <Job style={socialNetwork} title={'Social network'} description={'Social network for meeting people and communicating with them.'}/>
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
-export default Jobs;
+export default Jobs
