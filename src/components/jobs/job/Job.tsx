353c19b51f30244fe5ff26e9473e251e3b328758
AntohaBr <brelanton@gmail.com>
2023-04-22 12:23:08 +0300

    feat(jobs): add responsive design

--- src/components/jobs/job/Job.tsx
+++ src/components/jobs/job/Job.tsx
@@ -9,15 +9,22 @@ type JobPropsType = {
 
 export const Job: FC<JobPropsType> = ({style, title, description}) => {
     return (
-        <div className={s.job}>
-            <div className={s.picture} style={style}>
-                <button className={s.button}>Look</button>
-            </div>
-            <div className={s.jobInfo}>
-                <h3 className={s.jobTitle}>{title}</h3>
-                <div className={s.description}>{description}</div>
+        <div className={s.wrapper}>
+            <div className={s.job}>
+                <div className={s.picture} style={style}>
+                    <div>
+                        <button className={s.button}>Demo</button>
+                        <button className={s.button}>Code</button>
+                    </div>
+
+                </div>
+                <div className={s.jobInfo}>
+                    <h3 className={s.jobTitle}>{title}</h3>
+                    <div className={s.description}>{description}</div>
+                </div>
             </div>
         </div>
+
     )
 }
 
