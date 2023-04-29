b43e1aca0cb50b2f017c94b0e50c98f729572e75
AntohaBr <brelanton@gmail.com>
2023-04-29 13:17:18 +0300

    fix(skills): add skills (SASS, Axios, Rest API)

--- src/common/data/dataSkills.tsx
+++ src/common/data/dataSkills.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {
+    SvgIconAxios,
     SvgIconCss, SvgIconGit, SvgIconGitHub,
     SvgIconHtml,
     SvgIconJavaScript, SvgIconMaterialUi,
     SvgIconReact,
-    SvgIconRedux, SvgIconResponsiveDesign, SvgIconStoryBook,
+    SvgIconRedux, SvgIconResponsiveDesign, SvgIconSass, SvgIconStoryBook,
     SvgIconTypeScript, SvgIconUnitTest
 } from 'common/svgIcon'
 
@@ -15,10 +16,13 @@ export const dataSkills = [
     {id: 4, icon: <SvgIconJavaScript/>, title: 'JavaScript'},
     {id: 5, icon: <SvgIconHtml/>, title: 'HTML5'},
     {id: 6, icon: <SvgIconCss/>, title: 'CSS3'},
-    {id: 7, icon: <SvgIconGit/>, title: 'Git'},
-    {id: 8, icon: <SvgIconGitHub/>, title: 'GitHub'},
-    {id: 9, icon: <SvgIconMaterialUi/>, title: 'Material UI'},
-    {id: 10, icon: <SvgIconStoryBook/>, title: 'StoryBook'},
-    {id: 11, icon: <SvgIconUnitTest/>, title: 'Unit testing'},
-    {id: 12, icon: <SvgIconResponsiveDesign/>, title: 'Responsive design'},
+    {id: 7, icon: <SvgIconSass/>, title: 'SASS'},
+    {id: 8, icon: '<SvgIconCss/>', title: 'Rest API'},
+    {id: 9, icon: <SvgIconAxios/>, title: 'Axios'},
+    {id: 10, icon: <SvgIconGit/>, title: 'Git'},
+    {id: 11, icon: <SvgIconGitHub/>, title: 'GitHub'},
+    {id: 12, icon: <SvgIconMaterialUi/>, title: 'Material UI'},
+    {id: 13, icon: <SvgIconStoryBook/>, title: 'StoryBook'},
+    {id: 14, icon: <SvgIconUnitTest/>, title: 'Unit testing'},
+    {id: 15, icon: <SvgIconResponsiveDesign/>, title: 'Responsive design'},
 ]
